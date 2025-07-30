2698f0709d19c0137c9c31da604c1403b3ecbc29
NachoNeko2 <z196482963@163.com>
2025-07-30 10:22:22 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
 	//网页元数据
 	metaData: {
-		title: 'Leleo的个人主页🎉',
-		description: '欢迎来到Leleo的奇妙世界！',
-		keywords: 'Leleo,leleo,个人主页,个人网站',
+		title: 'Fatetech的个人主页🎉',
+		description: '欢迎来到Fate的奇妙世界！',
+		keywords: 'Fatetech,命运科技,个人主页,个人网站',
 		icon: '/favicon.ico'   //网页图标，支持外链
 	},
 
 	avatar: "/img/avatar.jpg", // 头像
-	welcometitle: "Hi, I'm Leleo", // 标题
+	welcometitle: "Hi, I'm Fate命定指冠", // 标题
 
 	// 颜色配置
 	color: {
@@ -51,8 +51,8 @@ const config = {
 
 	//社交按钮
 	socialPlatformIcons: [
-		{ icon: "mdi-github", link: "https://www.github.com/leleo886" },
-		{ icon: "mdi-email", link: "mailto:leleo886@foxmail.com" },
+		{ icon: "mdi-github", link: "https://www.github.com/NachoNeko2" },
+		{ icon: "mdi-email", link: "mailto:y196482963@163.com" },
 		{ icon: "mdi-qqchat", link: "https://im.qq.com/" },
 		{ icon: "mdi-wechat", link: "https://wx.qq.com/" },
 		{ icon: "mdi-youtube", link: "https://www.youtube.com" },
