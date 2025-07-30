0214ef839aafcac9d516a5a01af13b6eead2c715
NachoNeko2 <z196482963@163.com>
2025-07-30 11:09:40 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -146,7 +146,8 @@ const config = {
 		{ go: "🔍 前往", img: "/img/sunshine.jpg", title: "Project 8", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://leleo.top", show: false },
 	],
 
-	statement: ["备案号：XXICP备123456789号", "Copyright © 2025 Leleo"],
+	statement: ["备案号：<a href="https://icp.fatetech.top/id.php?keyword=88888888" target="_blank">
+       团ICP备88888888号</a>", "Copyright © 2025 Fate"],
 }
 
 export default config
